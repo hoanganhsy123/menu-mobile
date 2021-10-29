4c57486245bb3236aca239f502ff6a97966f3b10
Anh Sỹ Hoàng <hoanganhsy123@outlook.com>
2021-10-29 21:59:12 +0700

    Code onclick menu js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function showMenu(){
+
+    let menuLink = document.getElementById('myLinks');
+    if(menuLink.style.display == "block"){
+        menuLink.style.display = "none";
+    } else{
+        menuLink.style.display = "block";
+    }
+    console.log("haofnsg")
+
+//   document.getElementById('myLinks').style.display = "block";
+}
+
+
